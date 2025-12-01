5bfbde1fbde0ffa6a2293336d22165a8782bb3bf
inzamulhaque <mdihalif@gmail.com>
2025-12-01 12:22:42 +0600

    install react and antd

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Button type="primary">Blood Donor Management</Button>
+    </>
+  );
+}
+
+export default App;
