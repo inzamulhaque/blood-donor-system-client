aa2568346e70f04aa9b25d4543066dde4b1596fa
inzamulhaque <mdihalif@gmail.com>
2025-12-05 18:02:42 +0600

    create navbar

--- src/components/shared/navbar/Navbar.tsx
+++ src/components/shared/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Flex } from "antd";
 import "./Navbar.css";
 import Logo from "../../../assets/al-dima-logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -8,24 +9,67 @@ const Navbar = () => {
       <nav>
         <Flex
           gap={"middle"}
-          vertical
+          vertical={false}
           justify="space-between"
           align="center"
           style={{
-            maxWidth: "1200px",
+            maxWidth: "1000px",
             width: "100%",
-            height: "70px",
-
+            height: "60px",
             margin: "0 auto",
           }}
         >
-          <div className="logo">
-            <img
-              src={Logo}
-              alt="Al-Dima Logo"
-              style={{ height: "40px", marginRight: "10px" }}
-            />
-          </div>
+          <Flex
+            align="center"
+            justify="start"
+            style={{
+              height: "100%",
+            }}
+          >
+            <Link to={"/"}>
+              <img
+                src={Logo}
+                alt="Al-Dima Logo"
+                style={{
+                  height: "40px",
+                  marginRight: "10px",
+                  borderRadius: "5px",
+                }}
+              />
+            </Link>
+          </Flex>
+
+          {/* <Flex
+            align="center"
+            justify="space-between"
+            style={{
+              height: "100%",
+              fontSize: "18px",
+            }}
+          > */}
+          <Flex justify="space-around" gap={5}>
+            <Link to={"/"} className="menuItem">
+              Home
+            </Link>
+            <Link to={"/about"} className="menuItem">
+              About
+            </Link>
+            <Link to={"/our-team"} className="menuItem">
+              Our Team
+            </Link>
+            <Link to={"/contact"} className="menuItem">
+              Contact
+            </Link>
+          </Flex>
+          <Flex justify="space-between" gap={30}>
+            <Link to={"/signup"}>
+              <button className="navBtn navBtn-signup">SignUp</button>
+            </Link>
+            <Link to={"/signin"}>
+              <button className="navBtn navBtn-signin">SignIn</button>
+            </Link>
+          </Flex>
+          {/* </Flex> */}
         </Flex>
       </nav>
     </>
