0bfc119ab666786c8f0924fcfb42b5c38fe7b196
inzamulhaque <mdihalif@gmail.com>
2025-12-26 17:08:16 +0600

    create dashboard side bar and header

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -1,11 +1,12 @@
 import {
+  LogoutOutlined,
   MenuFoldOutlined,
   MenuUnfoldOutlined,
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
 } from "@ant-design/icons";
-import { Button, Layout, Menu } from "antd";
+import { Button, Flex, Layout, Menu } from "antd";
 import { Header } from "antd/es/layout/layout";
 import { useState } from "react";
 
@@ -34,27 +35,53 @@ const SideBar = () => {
 
   return (
     <>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="demo-logo-vertical" />
+      <Sider
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        style={{
+          height: "100vh",
+          position: "sticky",
+          left: 0,
+          top: 0,
+        }}
+      >
         <Header style={{ padding: 0 }}>
           <Button
             type="text"
             icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
             onClick={() => setCollapsed(!collapsed)}
             style={{
-              fontSize: "16px",
+              fontSize: 16,
               width: 64,
               height: 64,
+              color: "white",
             }}
           />
         </Header>
 
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["1"]}
-          items={menuItems}
-        />
+        <Flex vertical={true} style={{ height: "85vh" }}>
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            items={menuItems}
+            style={{ flex: 1 }}
+          />
+
+          <Menu
+            theme="dark"
+            mode="inline"
+            selectable={false}
+            items={[
+              {
+                key: "logout",
+                icon: <LogoutOutlined style={{ color: "red" }} />,
+                label: <span style={{ color: "red" }}>LogOut</span>,
+              },
+            ]}
+          />
+        </Flex>
       </Sider>
     </>
   );
--- src/components/layout/WithDashboardLayout.tsx
+++ src/components/layout/WithDashboardLayout.tsx
@@ -1,15 +1,37 @@
 import { AnimatePresence } from "motion/react";
 import SideBar from "./SideBar";
 import { Outlet } from "react-router-dom";
+import { Layout } from "antd";
+import { Header } from "antd/es/layout/layout";
+import logo from "../../assets/al-dima-logo.png";
 
 const WithDashboardLayout = () => {
   return (
     <>
-      <SideBar />
+      <Layout style={{ minHeight: "100vh" }}>
+        <SideBar />
 
-      <AnimatePresence mode="wait">
-        <Outlet />
-      </AnimatePresence>
+        <Layout>
+          <Header
+            style={{
+              background: "#fff",
+              paddingRight: "20px",
+              boxShadow: "0 2px 8px #f0f1f2",
+              textAlign: "right",
+            }}
+          >
+            <img
+              src={logo}
+              alt="Ihsan Al-Dima Logo"
+              style={{ height: "40px", verticalAlign: "middle" }}
+            />
+          </Header>
+
+          <AnimatePresence mode="wait">
+            <Outlet />
+          </AnimatePresence>
+        </Layout>
+      </Layout>
     </>
   );
 };
