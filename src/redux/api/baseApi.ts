94fa781b30e7f19e236c00812a4a749e223fa071
inzamulhaque <mdihalif@gmail.com>
2026-01-08 17:01:26 +0600

    use refresh token and get new token

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,21 +1,73 @@
-import { fetchBaseQuery } from "@reduxjs/toolkit/query";
+import {
+  DefinitionType,
+  fetchBaseQuery,
+  type BaseQueryApi,
+  type BaseQueryFn,
+  type FetchArgs,
+} from "@reduxjs/toolkit/query";
 import { createApi } from "@reduxjs/toolkit/query/react";
 import type { RootState } from "../store";
+import { toast } from "sonner";
+import { logout, setUser } from "../features/auth/authSlice";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:7000/api/v1",
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+
+    if (token) {
+      headers.set("authorization", token);
+    }
+
+    return headers;
+  },
+});
+
+const baseQueryWithRefreshToken: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+> = async (args, api, extraOptions): Promise<any> => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 404) {
+    toast.error(result?.error?.data?.message);
+  }
+
+  if (result?.error?.status === 403) {
+    toast.error(result?.error?.data?.message);
+  }
+
+  if (result?.error?.status === 401) {
+    const res = await fetch("http://localhost:7000/api/v1/auth/refresh-token", {
+      method: "POST",
+      credentials: "include",
+    });
+
+    const data = await res.json();
+
+    if (data?.data?.token) {
+      const user = (api.getState() as RootState).auth.user;
+
+      api.dispatch(
+        setUser({
+          user,
+          token: data?.data?.accessToken,
+        })
+      );
+
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logout());
+    }
+  }
+
+  return result;
+};
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:7000/api/v1",
-    credentials: "include",
-    prepareHeaders: (headers, { getState }) => {
-      const token = (getState() as RootState).auth.token;
-
-      if (token) {
-        headers.set("authorization", token);
-      }
-
-      return headers;
-    },
-  }),
+  baseQuery: baseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
